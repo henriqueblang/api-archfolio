7d48aaf605de1baf5a649f7fdcaf8b568c9ab71c
Henrique B Lang <henrique.barcia@gec.inatel.br>
2021-04-30 16:42:25 -0300

    feat: add users route

--- api/src/routers/v1/api.py
+++ api/src/routers/v1/api.py
@@ -1,6 +1,13 @@
 from fastapi import APIRouter
 from src.utils import routing
 
-hierarchical_routers = []
+from .endpoints import user
+
+hierarchical_routers = [
+    [
+        # Topmost level
+        (user.router, "/users", []),
+    ],
+]
 
 api_router = routing.build_hierarchical_routing(hierarchical_routers)
