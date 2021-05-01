3939996c402f086b14908765fd4d4234861d897a
Henrique B Lang <henrique.barcia@gec.inatel.br>
2021-05-01 16:55:06 -0300

    sql: fix update type fields

--- api/src/sql/user/update_user.sql
+++ api/src/sql/user/update_user.sql
@@ -2,8 +2,8 @@ UPDATE
     archfolio.users
 SET
     username = COALESCE(cast(:username AS TEXT), username),
-    salt = COALESCE(cast(:salt AS TEXT), salt),
-    password = COALESCE(cast(:password AS TEXT), password),
+    salt = COALESCE(cast(:salt AS BYTEA), salt),
+    password = COALESCE(cast(:password AS BYTEA), password),
     pfp_url = COALESCE(cast(:pfp_url AS TEXT), pfp_url),
     name = COALESCE(cast(:name AS TEXT), name),
     description = COALESCE(cast(:description AS TEXT), description),
