4b3fd1f1072ee2c45ce0fa686693e83d898a4c8f
Henrique B Lang <henrique.barcia@gec.inatel.br>
2021-05-10 12:13:11 -0300

    feat: add new endpoints route

--- api/src/routers/v1/api.py
+++ api/src/routers/v1/api.py
@@ -1,12 +1,21 @@
 from fastapi import APIRouter
 from src.utils import routing
 
-from .endpoints import user
+from .endpoints import follower, metadata, post, user
 
 hierarchical_routers = [
+    # 0
+    [
+        # Connects into hierarchical_routers[1][0] (/users)
+        (follower.router, "/{id}/followers", [0]),
+        # Connects into hierarchical_routers[1][1] (/posts)
+        (metadata.router, "/{post_id}/metadatas", [1]),
+    ],
+    # 1
     [
         # Topmost level
         (user.router, "/users", []),
+        (post.router, "/posts", []),
     ],
 ]
 
