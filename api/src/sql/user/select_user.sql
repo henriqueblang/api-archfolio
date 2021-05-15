0e892af8e1c796a889f84733e107746104c50e05
unknown <henrique.barcia@gec.inatel.br>
2021-05-15 17:10:40 -0300

    fix: fix select user query

--- api/src/sql/user/select_user.sql
+++ api/src/sql/user/select_user.sql
@@ -4,8 +4,10 @@ FROM
     archfolio.users
 WHERE
     (cast(:id AS INT) IS NULL OR id = :id)
-    OR
-    (cast(:identification AS TEXT) IS NULL OR username = :identification)
-    OR
-    (cast(:identification AS TEXT) IS NULL OR email = :identification)
+    AND
+    (
+        (cast(:identification AS TEXT) IS NULL OR username = :identification)
+        OR
+        (cast(:identification AS TEXT) IS NULL OR email = :identification)
+    )
 ORDER BY (SELECT NULL) OFFSET :offset ROWS FETCH NEXT :fetch ROWS ONLY
--- api/src/sql/user/select_user_no_pagination.sql
+++ api/src/sql/user/select_user_no_pagination.sql
@@ -4,7 +4,9 @@ FROM
     archfolio.users
 WHERE
     (cast(:id AS INT) IS NULL OR id = :id)
-    OR
-    (cast(:identification AS TEXT) IS NULL OR username = :identification)
-    OR
-    (cast(:identification AS TEXT) IS NULL OR email = :identification)
+    AND
+    (
+        (cast(:identification AS TEXT) IS NULL OR username = :identification)
+        OR
+        (cast(:identification AS TEXT) IS NULL OR email = :identification)
+    )
