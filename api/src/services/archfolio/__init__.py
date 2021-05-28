00da9aa99f5a19ea3bd2d0fd3cefedef1ac601fa
Henrique B Lang <henrique.barcia@gec.inatel.br>
2021-05-28 15:47:05 -0300

    feat: enable new service methods

--- api/src/services/archfolio/__init__.py
+++ api/src/services/archfolio/__init__.py
@@ -7,7 +7,10 @@ from src.repository.database import Database
 class Archfolio(Database):
     client = Api(access_token=settings.settings.KEYS["GYAZO"])
 
+    from ._comment import create_comment, delete_comments, get_comments
+    from ._favorite import delete_favorites, favorite_post, get_favorites
     from ._follower import delete_followers, follow_user, get_followers
+    from ._like import delete_likes, get_likes, like_post
     from ._metadata import (
         create_metadata,
         delete_metadatas,
