7371ea0843f112cb5bd48f0283778ce15a58ec87
Henrique B Lang <henrique.barcia@gec.inatel.br>
2021-05-28 15:47:15 -0300

    feat: add new routes

--- api/src/routers/v1/api.py
+++ api/src/routers/v1/api.py
@@ -1,15 +1,21 @@
 from fastapi import APIRouter
 from src.utils import routing
 
-from .endpoints import follower, metadata, post, user
+from .endpoints import comment, favorite, follower, like, metadata, post, user
 
 hierarchical_routers = [
     # 0
     [
         # Connects into hierarchical_routers[1][0] (/users)
-        (follower.router, "/{id}/followers", [0]),
+        (follower.router, "/{user_id}/followers", [0]),
         # Connects into hierarchical_routers[1][1] (/posts)
         (metadata.router, "/{post_id}/metadatas", [1]),
+        # Connects into hierarchical_routers[1][1] (/posts)
+        (comment.router, "/{post_id}/comments", [1]),
+        # Connects into hierarchical_routers[1][1] (/posts)
+        (favorite.router, "/{post_id}/favorites", [1]),
+        # Connects into hierarchical_routers[1][1] (/posts)
+        (like.router, "/{post_id}/likes", [1]),
     ],
     # 1
     [
